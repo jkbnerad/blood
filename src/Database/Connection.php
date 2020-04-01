72c8b09b6650bb8398ee285c717874f5abfd0e55
Jakub Nerad <jakubnerad@gmail.com>
2020-04-01 18:56:10 +0200

    Support for Mysql SSL connection

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -13,6 +13,9 @@ class Connection
     public function __construct()
     {
         $config = parse_ini_file(__DIR__ . '/../../configs/database.ini') ?: [];
+        if (strpos($config['dsn'], '.azure.com') !== false) { // set SSL for Azure
+            $config['options'][\PDO::MYSQL_ATTR_SSL_CA] = '/var/www/html/krev-dev/configs/BaltimoreCyberTrustRoot.crt.pem';
+        }
         $this->connection = new \Dibi\Connection($config);
     }
 
